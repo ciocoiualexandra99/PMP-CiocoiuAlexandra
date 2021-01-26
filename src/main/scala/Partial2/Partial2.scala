02ec027f8a3cb806028c35a3d068b73a7c894ff2
ciocoiualexandra99 <61777325+ciocoiualexandra99@users.noreply.github.com>
2021-01-26 09:17:49 +0200

    Add files via upload

--- src/main/scala/Partial2/Partial2.scala
+++ src/main/scala/Partial2/Partial2.scala
@@ -4,6 +4,21 @@ import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
 
 object Ex1 {
+
+abstract class State {
+val Ploios: Element[Boolean]
+val Start : Int
+val Insorit: Int
+val Innorat :Int
+
+def vreme: Element[Boolean] =
+If()
+}
+class InitialState()
+      extends State {
+    val Start=
+  }
+
 	def main(args: Array[String]) {
 		val test = Constant("Test")
 
